ce8b9cf2f660b982b5fecd5c46210a704378289d
AyushiAnderiya <ayushianderiya14@gmail.com>
2024-05-08 19:20:06 +0530

    Language Translator

---
+++ languages.js
@@ -0,0 +1,297 @@
+const languages = [
+  {
+    no: "0",
+    name: "Auto",
+    code: "auto",
+  },
+  {
+    no: "1",
+    name: "Afrikaans",
+    code: "af",
+  },
+  {
+    no: "2",
+    name: "Albanian",
+    code: "sq",
+  },
+  {
+    no: "3",
+    name: "Arabic",
+    code: "ar",
+  },
+  {
+    no: "4",
+    name: "Armenian",
+    code: "hy",
+  },
+  {
+    no: "5",
+    name: "Azerbaijani",
+    code: "az",
+  },
+  {
+    no: "6",
+    name: "Basque",
+    code: "eu",
+  },
+  {
+    no: "7",
+    name: "Belarusian",
+    code: "be",
+  },
+  {
+    no: "8",
+    name: "Bulgarian",
+    code: "bg",
+  },
+  {
+    no: "9",
+    name: "Catalan",
+    code: "ca",
+  },
+  {
+    no: "10",
+    name: "Chinese (Simplified)",
+    code: "zh-CN",
+  },
+  {
+    no: "11",
+    name: "Chinese (Traditional)",
+    code: "zh-TW",
+  },
+  {
+    no: "12",
+    name: "Croatian",
+    code: "hr",
+  },
+  {
+    no: "13",
+    name: "Czech",
+    code: "cs",
+  },
+  {
+    no: "14",
+    name: "Danish",
+    code: "da",
+  },
+  {
+    no: "15",
+    name: "Dutch",
+    code: "nl",
+  },
+  {
+    no: "16",
+    name: "English",
+    code: "en",
+  },
+  {
+    no: "17",
+    name: "Estonian",
+    code: "et",
+  },
+  {
+    no: "18",
+    name: "Filipino",
+    code: "tl",
+  },
+  {
+    no: "19",
+    name: "Finnish",
+    code: "fi",
+  },
+  {
+    no: "20",
+    name: "French",
+    code: "fr",
+  },
+  {
+    no: "21",
+    name: "Galician",
+    code: "gl",
+  },
+  {
+    no: "22",
+    name: "Georgian",
+    code: "ka",
+  },
+  {
+    no: "23",
+    name: "German",
+    code: "de",
+  },
+  {
+    no: "24",
+    name: "Greek",
+    code: "el",
+  },
+  {
+    no: "25",
+    name: "Haitian Creole",
+    code: "ht",
+  },
+  {
+    no: "26",
+    name: "Hebrew",
+    code: "iw",
+  },
+  {
+    no: "27",
+    name: "Hindi",
+    code: "hi",
+  },
+  {
+    no: "28",
+    name: "Hungarian",
+    code: "hu",
+  },
+  {
+    no: "29",
+    name: "Icelandic",
+    code: "is",
+  },
+  {
+    no: "30",
+    name: "Indonesian",
+    code: "id",
+  },
+  {
+    no: "31",
+    name: "Irish",
+    code: "ga",
+  },
+  {
+    no: "32",
+    name: "Italian",
+    code: "it",
+  },
+  {
+    no: "33",
+    name: "Japanese",
+    code: "ja",
+  },
+  {
+    no: "34",
+    name: "Korean",
+    code: "ko",
+  },
+  {
+    no: "35",
+    name: "Latvian",
+    code: "lv",
+  },
+  {
+    no: "36",
+    name: "Lithuanian",
+    code: "lt",
+  },
+  {
+    no: "37",
+    name: "Macedonian",
+    code: "mk",
+  },
+  {
+    no: "38",
+    name: "Malay",
+    code: "ms",
+  },
+  {
+    no: "39",
+    name: "Maltese",
+    code: "mt",
+  },
+  {
+    no: "40",
+    name: "Norwegian",
+    code: "no",
+  },
+  {
+    no: "41",
+    name: "Persian",
+    code: "fa",
+  },
+  {
+    no: "42",
+    name: "Polish",
+    code: "pl",
+  },
+  {
+    no: "43",
+    name: "Portuguese",
+    code: "pt",
+  },
+  {
+    no: "44",
+    name: "Romanian",
+    code: "ro",
+  },
+  {
+    no: "45",
+    name: "Russian",
+    code: "ru",
+  },
+  {
+    no: "46",
+    name: "Serbian",
+    code: "sr",
+  },
+  {
+    no: "47",
+    name: "Slovak",
+    code: "sk",
+  },
+  {
+    no: "48",
+    name: "Slovenian",
+    code: "sl",
+  },
+  {
+    no: "49",
+    name: "Spanish",
+    code: "es",
+  },
+  {
+    no: "50",
+    name: "Swahili",
+    code: "sw",
+  },
+  {
+    no: "51",
+    name: "Swedish",
+    code: "sv",
+  },
+  {
+    no: "52",
+    name: "Thai",
+    code: "th",
+  },
+  {
+    no: "53",
+    name: "Turkish",
+    code: "tr",
+  },
+  {
+    no: "54",
+    name: "Ukrainian",
+    code: "uk",
+  },
+  {
+    no: "55",
+    name: "Urdu",
+    code: "ur",
+  },
+  {
+    no: "56",
+    name: "Vietnamese",
+    code: "vi",
+  },
+  {
+    no: "57",
+    name: "Welsh",
+    code: "cy",
+  },
+  {
+    no: "58",
+    name: "Yiddish",
+    code: "yi",
+  },
+];
